be8e86d2cd3f47bbaa4a29bbe494fe3ff6e4624a
gdev522 <145327557+gdev522@users.noreply.github.com>
2024-04-11 00:15:39 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dummy_package',
+    version='0.1',
+    packages=find_packages(),
+    description='A dummy Python package for testing',
+)
